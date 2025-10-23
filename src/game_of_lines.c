00f52cff87d3889d331b87b55908933facd93448
Matei Tudor Brezoi <136821484+MateiTB23@users.noreply.github.com>
2025-10-23 21:26:06 +0200

    Initial commit

---
+++ src/game_of_lines.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "game_of_lines.h"
+
+// Implement functions defined in game_of_lines.h
---
+++ test/test_game_of_lines.c
@@ -0,0 +1,10 @@
+#include "mtest.h"
+#include "game_of_lines.h"
+
+// TODO: Write unit tests!
+TEST_CASE(test_name,
+    CHECK_FAIL("Test is not yet implemented!");
+)
+
+
+MAIN_RUN_TESTS(test_name)
